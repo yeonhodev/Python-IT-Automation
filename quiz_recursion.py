463242b2fc63e6f01df452c5262749490ec3f85c
yeonhodev <yeonho.dev@gmail.com>
2021-02-26 23:27:55 -0500

    Update quiz_recursion.py

--- quiz_recursion.py
+++ quiz_recursion.py
@@ -13,3 +13,20 @@ def is_power_of(number, base):
 print(is_power_of(8,2)) # Should be True
 print(is_power_of(64,4)) # Should be True
 print(is_power_of(70,10)) # Should be False
+
+
+# Question 4
+# The count_users function recursively counts the amount of users that belong to a group in the company system, by going through each of the members of a group and if one of them is a group, recursively calling the function and counting the members. But it has a bug! Can you spot the problem and fix it?
+
+def count_users(group):
+  count = 0
+  for member in get_members(group):
+    if is_group(member):
+      count += count_users(member)
+    else:
+      count += 1
+  return count
+
+print(count_users("sales")) # Should be 3
+print(count_users("engineering")) # Should be 8
+print(count_users("everyone")) # Should be 18
