e113c7af39e93c0352ca1c1f7b1b274db8c31153
yeonhodev <yeonho.dev@gmail.com>
2021-02-26 23:42:25 -0500

    Update quiz_recursion.py

--- quiz_recursion.py
+++ quiz_recursion.py
@@ -30,3 +30,17 @@ def count_users(group):
 print(count_users("sales")) # Should be 3
 print(count_users("engineering")) # Should be 8
 print(count_users("everyone")) # Should be 18
+
+
+# Question 5
+# Implement the sum_positive_numbers function, as a recursive function that returns the sum of all positive numbers between the number n received and 1. For example, when n is 3 it should return 1+2+3=6, and when n is 5 it should return 1+2+3+4+5=15.
+
+def sum_positive_numbers(n):
+  if n <= 1:
+    return n
+  else:
+    return n + sum_positive_numbers(n-1)
+
+
+print(sum_positive_numbers(3)) # Should be 6
+print(sum_positive_numbers(5)) # Should be 15
