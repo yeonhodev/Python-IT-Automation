b02b6d55650720d1323128c360c9d814f65f861f
yeonhodev <yeonho.dev@gmail.com>
2021-02-28 23:22:51 -0500

    Update module3_graded_quiz.py

--- module3_graded_quiz.py
+++ module3_graded_quiz.py
@@ -5,3 +5,30 @@ number = 1
 while number <= 7:
 	print(number, end=" ")
 	number += 1
+
+# Question 2
+# The show_letters function should print out each letter of a word on a separate line. Fill in the blanks to make that happen.
+
+def show_letters(word):
+	for i in word:
+		print(i, end='\n')
+
+show_letters("Hello")
+# Should print one line per letter
+
+# Question 3
+# Complete the function digits(n) that returns how many digits the number has. For example: 25 has 2 digits and 144 has 3 digits. Tip: you can figure out the digits of a number by dividing it by 10 once per digit until there are no digits left.
+
+def digits(n):
+	count = 0
+	if n == 0:
+	  return 1
+	while (n > 0):
+		count += 1
+		n = n//10
+	return count
+	
+print(digits(25))   # Should print 2
+print(digits(144))  # Should print 3
+print(digits(1000)) # Should print 4
+print(digits(0))    # Should print 1
