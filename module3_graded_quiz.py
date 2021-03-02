cdc4a033e625751b33d61f77d8c24591a6564c44
yeonhodev <yeonho.dev@gmail.com>
2021-03-01 23:52:49 -0500

    Update module3_graded_quiz.py

--- module3_graded_quiz.py
+++ module3_graded_quiz.py
@@ -32,3 +32,21 @@ print(digits(25))   # Should print 2
 print(digits(144))  # Should print 3
 print(digits(1000)) # Should print 4
 print(digits(0))    # Should print 1
+
+
+# Question 4
+"""
+This function prints out a multiplication table (where each number is the result of multiplying the first number of its row by the number at the top of its column). Fill in the blanks so that calling multiplication_table(1, 3) will print out:
+1 2 3 
+2 4 6 
+3 6 9
+"""
+
+def multiplication_table(start, stop):
+	for x in range(start, stop + 1):
+		for y in range(start, stop + 1):
+			print(str(x*y), end=" ")
+		print()
+
+multiplication_table(1, 3)
+# Should print the multiplication table shown above
