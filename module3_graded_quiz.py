c62eed2e14c130e7b026d621eb78263d1e34ba2f
yeonhodev <yeonho.dev@gmail.com>
2021-03-03 00:05:31 -0500

    Update module3_graded_quiz.py

--- module3_graded_quiz.py
+++ module3_graded_quiz.py
@@ -75,3 +75,19 @@ def counter(start, stop):
 print(counter(1, 10)) # Should be "Counting up: 1,2,3,4,5,6,7,8,9,10"
 print(counter(2, 1)) # Should be "Counting down: 2,1"
 print(counter(5, 5)) # Should be "Counting up: 5"
+
+# Question 6
+# The even_numbers function returns a space-separated string of all positive numbers that are divisible by 2, up to and including the maximum that's passed into the function. For example, even_numbers(6) returns “2 4 6”. Fill in the blank to make this work.
+
+def even_numbers(maximum):
+	return_string = ""
+	for x in range(2, maximum+1, 2):
+		return_string += str(x) + " "
+	return return_string.strip()
+
+print(even_numbers(6))  # Should be 2 4 6
+print(even_numbers(10)) # Should be 2 4 6 8 10
+print(even_numbers(1))  # No numbers displayed
+print(even_numbers(3))  # Should be 2
+print(even_numbers(0))  # No numbers displayed
+
