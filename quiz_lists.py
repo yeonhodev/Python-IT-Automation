ea4ea2a982b270d2532789426a571339b42c4d92
yeonhodev <yeonho.dev@gmail.com>
2021-03-07 01:03:32 -0500

    Update quiz_lists.py

--- quiz_lists.py
+++ quiz_lists.py
@@ -17,3 +17,21 @@ for filename in filenames:
 
 print(newfilenames) 
 # Should be ["program.c", "stdio.h", "sample.h", "a.out", "math.h", "hpp.out"]
+
+
+# Question 2
+# Let's create a function that turns text into pig latin: a simple text transformation that modifies each word moving the first character to the end and appending "ay" to the end. For example, python ends up as ythonpay.
+
+def pig_latin(text):
+  say = ""
+  # Separate the text into words
+  words = text.split()
+  for word in words:
+    # Create the pig latin word and add it to the list
+    pig_latin_word = "{}{}ay".format(word[1:], word[:1], )
+    # Turn the list back into a phrase
+    say = say + " " + pig_latin_word
+  return say
+		
+print(pig_latin("hello how are you")) # Should be "ellohay owhay reaay ouyay"
+print(pig_latin("programming in python is fun")) # Should be "rogrammingpay niay ythonpay siay unfay"
