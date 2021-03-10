f018dd399d3d2a1d3732a0cc70d3d63dc880abde
yeonhodev <yeonho.dev@gmail.com>
2021-03-10 00:02:09 -0500

    Update quiz_dictionaries.py

--- quiz_dictionaries.py
+++ quiz_dictionaries.py
@@ -5,3 +5,16 @@ wardrobe = {"shirt":["red","blue","white"], "jeans":["blue","black"]}
 for item, color in wardrobe.items():
 	for c in color:
 		print("{} {}".format(c, item))
+
+# Question 1
+# The email_list function receives a dictionary, which contains domain names as keys, and a list of users as values. Fill in the blanks to generate a list that contains complete email addresses (e.g. diana.prince@gmail.com).
+
+def email_list(domains):
+	emails = []
+	for domain, users in domains.items():
+	  for user in users:
+	    emails.append("{}@{}".format(user, domain))
+	return(emails)
+
+print(email_list({"gmail.com": ["clark.kent", "diana.prince", "peter.parker"], "yahoo.com": ["barbara.gordon", "jean.grey"], "hotmail.com": ["bruce.wayne"]}))
+
