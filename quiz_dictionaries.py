3b44aaa646d801d56ede5705370a5a345254f844
yeonhodev <yeonho.dev@gmail.com>
2021-03-09 23:54:04 -0500

    Create quiz_dictionaries.py

---
+++ quiz_dictionaries.py
@@ -0,0 +1,7 @@
+# Question
+# In Python, a dictionary can only hold a single value for a given key. To workaround this, our single value can be a list containing multiple values. Here we have a dictionary called "wardrobe" with items of clothing and their colors. Fill in the blanks to print a line for each item of clothing with each color, for example: "red shirt", "blue shirt", and so on.
+
+wardrobe = {"shirt":["red","blue","white"], "jeans":["blue","black"]}
+for item, color in wardrobe.items():
+	for c in color:
+		print("{} {}".format(c, item))
