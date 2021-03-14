c86a40ee148302282a329bb0604789c1918d0fb6
yeonhodev <yeonho.dev@gmail.com>
2021-03-13 23:58:19 -0500

    Update quiz_dictionaries.py

--- quiz_dictionaries.py
+++ quiz_dictionaries.py
@@ -18,3 +18,5 @@ def email_list(domains):
 
 print(email_list({"gmail.com": ["clark.kent", "diana.prince", "peter.parker"], "yahoo.com": ["barbara.gordon", "jean.grey"], "hotmail.com": ["bruce.wayne"]}))
 
+# Question 2
+# The groups_per_user function receives a dictionary, which contains group names with the list of users. Users can belong to multiple groups. Fill in the blanks to return a dictionary with the users as keys and a list of their groups as values. 
